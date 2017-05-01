b77440158b126b472ae42dc824e0c463a6b365ed
Tsvetan Ovedenski <dev@tsovedenski.com>
2017-05-01 11:02:09 +0200

    Refactor array of arrays to linked hashmap

--- src/main/kotlin/com/tsovedenski/ai/draughts/game/State.kt
+++ src/main/kotlin/com/tsovedenski/ai/draughts/game/State.kt
@@ -3,12 +3,13 @@ package com.tsovedenski.ai.draughts.game
 import com.tsovedenski.ai.draughts.game.elements.*
 import com.tsovedenski.ai.draughts.players.Player
 import java.util.*
+import kotlin.collections.LinkedHashMap
 
 /**
  * Created by Tsvetan Ovedenski on 30/04/2017.
  */
 
-data class State (private val board: Array<Array<Cell>>, val size: Int, val pieces: Int) {
+data class State (private val board: LinkedHashMap<Point, Cell>, val size: Int, val pieces: Int) {
     constructor(size: Int, pieces: Int): this(generate(size, pieces), size, pieces)
 
     init {
@@ -18,19 +19,23 @@ data class State (private val board: Array<Array<Cell>>, val size: Int, val piec
     }
 
     fun valid(move: Move): Boolean {
-        if (!board[move.from].allowed || !board[move.to].allowed) {
+        if (!board.containsKey(move.from) || !board.containsKey(move.to)) {
             return false
         }
 
-        if (board[move.from].piece == null) {
+        if (!board[move.from]!!.allowed || !board[move.to]!!.allowed) {
             return false
         }
 
-        if (board[move.to].piece != null) {
+        if (board[move.from]!!.piece == null) {
             return false
         }
 
-        if (board[move.from].piece!!.color != move.player.color) {
+        if (board[move.to]!!.piece != null) {
+            return false
+        }
+
+        if (board[move.from]!!.piece!!.color != move.player.color) {
             return false
         }
 
@@ -42,12 +47,12 @@ data class State (private val board: Array<Array<Cell>>, val size: Int, val piec
             throw RuntimeException("Illegal move $move")
         }
 
-        val cloned = copy(board = board.map { it.clone() }.toTypedArray())
+        val cloned = copy(board = LinkedHashMap(board))
 
-        val piece = cloned.board[move.from].piece
-        cloned.board[move.from] = cloned.board[move.from].copy(piece = null)
+        val piece = cloned.board[move.from]!!.piece
+        cloned.board[move.from] = cloned.board[move.from]!!.copy(piece = null)
 
-        cloned.board[move.to] = cloned.board[move.to].copy(piece = piece)
+        cloned.board[move.to] = cloned.board[move.to]!!.copy(piece = piece)
 
         return cloned
     }
@@ -73,8 +78,10 @@ data class State (private val board: Array<Array<Cell>>, val size: Int, val piec
 
         builder.appendln("row${divider.substring(3)}")
 
-        board.forEachIndexed { index, row ->
-            builder.appendln("${String.format("%1$3d", index)}| ${row.map {
+        for (i in 0..size*size-1 step size) {
+            val row = board.keys.toList().subList(i, i+size)
+            builder.appendln("${String.format("%1$3d", i / size)}| ${row.map { point ->
+                val it = board[point]!!
                 when (it.allowed) {
                     true -> it.piece?.color?.char ?: '░'
                     else -> ' '
@@ -82,7 +89,7 @@ data class State (private val board: Array<Array<Cell>>, val size: Int, val piec
             }.joinToString(" | ")} |")
             builder.appendln(divider)
         }
-
+        
         builder.append("col| ${(0..Math.min(9, size-1)).toList().joinToString(" | ")} |")
         if (size > 10) {
             builder.append(" ${(10..size-1).toList().joinToString("| ")}|")
@@ -92,41 +99,18 @@ data class State (private val board: Array<Array<Cell>>, val size: Int, val piec
         return builder.toString()
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other?.javaClass != javaClass) return false
-
-        other as State
-        if (!Arrays.equals(board, other.board)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return Arrays.hashCode(board)
-    }
-
     companion object {
         var cnt = 0
 
-        internal operator fun Array<Array<Cell>>.get(p: Point) = this[p.row][p.col]
-        internal operator fun Array<Array<Cell>>.set(p: Point, c: Cell) {
-            this[p.row][p.col] = c
-        }
+        private fun generate(size: Int, pieces: Int) = LinkedHashMap<Point, Cell>().apply {
+            (0..size-1).forEach { row ->
+                            (0..size-1).forEach { col ->
+                                val point = Point(row, col)
+                                val cell  = Cell.fromPosition(size, pieces, row, col)
 
-        private fun generate(size: Int, pieces: Int) =
-                (0..size-1)
-                        .map { row ->
-                            (0..size-1).map { col ->
-                                val allowed = (row * size + col + 1) and 1 == row and 1
-                                val piece = when {
-                                    allowed && row < pieces / (size / 2) -> Piece(color = Color.Black)
-                                    allowed && row >= size - pieces / (size / 2) -> Piece(color = Color.White)
-                                    else -> null
-                                }
-                                Cell(piece = piece, allowed = allowed)
-                            }.toTypedArray()
+                                put(point, cell)
+                            }
                         }
-                        .toTypedArray()
+        }
     }
 }
--- src/main/kotlin/com/tsovedenski/ai/draughts/game/elements/Cell.kt
+++ src/main/kotlin/com/tsovedenski/ai/draughts/game/elements/Cell.kt
@@ -9,4 +9,17 @@ data class Cell(val allowed: Boolean, val piece: Piece? = null) {
             throw RuntimeException("Piece put on not allowed cell")
         }
     }
+    companion object {
+        fun fromPosition(boardSize: Int, boardPieces: Int, row: Int, col: Int): Cell {
+            val allowed = (row * boardSize + col + 1) and 1 == row and 1
+
+            val piece = when {
+                allowed && row < boardPieces / (boardSize / 2) -> Piece(color = Color.Black)
+                allowed && row >= boardSize - boardPieces / (boardSize / 2) -> Piece(color = Color.White)
+                else -> null
+            }
+
+            return Cell(piece = piece, allowed = allowed)
+        }
+    }
 }
